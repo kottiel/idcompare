55f375a3057e1ed5ffb96de0fc8cc715b4ed1ec1
kottiel <kottiel@gmail.com>
2019-08-13 07:15:46 -0400

    Moved idoc manipulation functions to idoc.c

--- idoc.h
+++ idoc.h
@@ -5,13 +5,14 @@
 #ifndef IDCOMPARE_IDOC_H
 #define IDCOMPARE_IDOC_H
 
+#include <stdio.h>
+
 #define MED            31
 #define LBL            10
 #define MAX_ROW_LEN  1000
 #define TYPE            7
 #define ATTR_NAME      63
-
-
+#define START_SIZE     10
 
 typedef struct {
     char pcode[MED];
@@ -19,5 +20,11 @@ typedef struct {
     char attr_name[MED];
     char attr_val[MED];
 } Idoc_row;
+
+size_t strlcpy(char *dst, const char *src, size_t dsize);
+
+size_t strlcpy_sp(char *dst, const char *src, size_t dsize);
+
+int read_idoc(Idoc_row *idoc, FILE *fp);
 
 #endif //IDCOMPARE_IDOC_H
